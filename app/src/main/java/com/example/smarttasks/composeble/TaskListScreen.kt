3b5153f7ae278c564a6c59362c84ad7734666dfc
dimitrijemijatovic <mijatovicdimitrije@gmaill.com>
2024-11-17 18:06:59 +0100

    adds empty screen image.

--- app/src/main/java/com/example/smarttasks/composeble/TaskListScreen.kt
+++ app/src/main/java/com/example/smarttasks/composeble/TaskListScreen.kt
@@ -1,5 +1,7 @@
 package com.example.smarttasks.composeble
 
+import com.example.smarttasks.R
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -28,6 +30,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -141,7 +145,10 @@ fun EmptyScreen(
             .fillMaxSize(),
         contentAlignment = Alignment.Center,
     ) {
-        Text(text = "Empty task list.")
+        Image(
+            painter = painterResource(id = R.drawable.empty_screen),
+            contentDescription = "Description",
+        )
     }
 }
 
